fd4fa29b68ec27d59af3d20838ebec1fd4bff47c
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-05 14:27:08 -0400

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+let colors = ["red", "blue", "green", "black"];
+
+for (let i = 0; i < colors.length; i++){
+    console.log(colors[i])
+    document.write(colors[i])
+}
+
+for (let color of colors){
+    console.log(color)
+    document.write(color)
+}
